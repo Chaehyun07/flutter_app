21755cd248b6c48b95541396a7f904d89fc392d4
603-06\kccistc <shooin1025@gmail.com>
2024-07-29 17:56:30 +0900

    1일차 마지막 커밋

--- lib/plash.dart
+++ lib/plash.dart
@@ -10,13 +10,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-    body: container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      color: Colors.grey,
-      
-    )
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("안녕하세요"),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.grey 
+      )
     );
   }
 }
